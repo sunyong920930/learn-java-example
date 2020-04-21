05d3f6fbec1a1e54fe44dee21463bb233d4a7907
黑夜的雨 <1052243813@qq.com>
2020-04-21 11:17:24 +0800

    ceshi

--- src/main/java/lean/java/example/Application.java
+++ src/main/java/lean/java/example/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.currentTimeMillis();
         SpringApplication.run(Application.class, args);
     }
 }
